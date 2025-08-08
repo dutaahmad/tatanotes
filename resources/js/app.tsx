5574c0e7e8ec1321469a5a892689defa06005338
Nahdi Duta Ahmad <dutaahmadtefur@gmail.com>
2025-08-08 15:26:18 +0700

    feat: now supports darkmode, custom theme toggle

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -5,6 +5,8 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 import { initializeTheme } from './hooks/use-appearance';
 
+import { ThemeProvider } from "@/components/theme-provider";
+
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
@@ -13,7 +15,11 @@ createInertiaApp({
     setup({ el, App, props }) {
         const root = createRoot(el);
 
-        root.render(<App {...props} />);
+        root.render(
+            <ThemeProvider>
+                <App {...props} />
+            </ThemeProvider>
+        );
     },
     progress: {
         color: '#4B5563',
